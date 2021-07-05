b8029a5de39dc342a580ae18cc1a0b436f764887
loubna <63347877+loubna100@users.noreply.github.com>
2021-07-05 21:06:31 +0100

    Update 0-subs.py

--- 0x06-api_advanced/0-subs.py
+++ 0x06-api_advanced/0-subs.py
@@ -4,17 +4,14 @@ get number of subscribers for a subredit
 """
 import requests
 
-
 def number_of_subscribers(subreddit):
     
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
     # get user agent
     # https://stackoverflow.com/questions/10606133/ -->
     # sending-user-agent-using-requests-library-in-python
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
-
     r = requests.get(url, headers=headers).json()
     subscribers = r.get('data', {}).get('subscribers')
     if not subscribers:
