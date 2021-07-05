38a21c33b9f1b9f121c5f0f8d23bdf607757fb7e
loubna <63347877+loubna100@users.noreply.github.com>
2021-07-05 21:48:14 +0100

    Update 0-subs.py

--- 0x06-api_advanced/0-subs.py
+++ 0x06-api_advanced/0-subs.py
@@ -5,9 +5,7 @@ import requests
 def number_of_subscribers(subreddit):
     
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    # get user agent
-    # https://stackoverflow.com/questions/10606133/ -->
-    # sending-user-agent-using-requests-library-in-python
+   
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
     r = requests.get(url, headers=headers).json()
