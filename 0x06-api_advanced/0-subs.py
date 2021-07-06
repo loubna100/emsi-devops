5a15c581f7cba7b969dc1a4729db67d000fe36ca
loubna <63347877+loubna100@users.noreply.github.com>
2021-07-06 19:21:10 +0100

    Update 0-subs.py

--- 0x06-api_advanced/0-subs.py
+++ 0x06-api_advanced/0-subs.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
+
+"""[SUBREDDIT]
 """
-Query Reddit API for number of subscribers for a given subreddit
-"""
-import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-        return number of subscribers for a given subreddit
-        return 0 if invalid subreddit given
-    """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    import requests
 
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    req = requests.get(url,
+                       headers={'User-Agent': 'My User Agent 1.0'},
+                       allow_redirects=False)
 
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
-
-    r = requests.get(url, headers=headers).json()
-    subscribers = r.get('data', {}).get('subscribers')
-    if not subscribers:
+    if req.status_code != 200:
         return 0
-    return subscribers
+    return req.json()['data']['subscribers']
